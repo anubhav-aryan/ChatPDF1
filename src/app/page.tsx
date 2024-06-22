155abb2882f1e131a518d99f18eba81f8de7010e
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2024-06-22 13:53:04 +0530

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { UserButton, useAuth } from '@clerk/nextjs'
+import React, { useEffect, useState } from 'react'
+import { ArrowRight, LogIn } from "lucide-react";
 
 export default async function Home() {
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <div className="flex flex-col items-center text-center">
+       
         <div className="flex items-center">
+        <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div> 
+
+          <div className="flex mt-3">
+          <Button>Go to Chats <ArrowRight className="ml-2" /></Button>
+          </div>
+          
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students, researchers and professionals to instantly
+            answer questions and understand research with AI
+          </p>
+
+          <div className="w-full mt-4">
+              <h1> File Upload</h1>
           </div>
           </div>
           </div>
