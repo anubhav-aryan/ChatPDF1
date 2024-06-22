b4c1cc042939212c40630a2b369620dfc5972cb6
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2024-06-22 22:11:52 +0530

    feat: pinecone db

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { SignIn } from '@clerk/nextjs'
 const page = () => {
   return (
-    <SignIn/>
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <SignIn />
+  </div>
   )
 }
 
