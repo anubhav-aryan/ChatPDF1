e3b51dec1a5bfa8530bd5c8e8df5efd9b0d46cb1
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2024-06-24 23:19:21 +0530

    fix: pinecone setup

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -42,7 +42,7 @@ const FileUpload = () => {
           try{
           setUploading(true);
           const data = await uploadToS3(file);
-          if(!data?.file_key || !data?.file_name){
+          if(!data?.file_key || !data.file_name){
             toast.error("Something went wrong! Please try again later");
             return;
           }
