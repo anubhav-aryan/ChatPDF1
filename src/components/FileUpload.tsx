cf843b9bbcebeeee64b7bb21b83ce480de9c7ef2
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2024-06-24 17:03:27 +0530

    feat: connected to S3 bucket

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -2,13 +2,30 @@
 import React from 'react'
 import { useDropzone } from 'react-dropzone';
 import { Inbox } from 'lucide-react';
+import { uploadToS3 } from '@/lib/s3';
+
 
 const FileUpload = () => {
     const { getRootProps, getInputProps } = useDropzone({
         accept: { "application/pdf": [".pdf"] },
         maxFiles: 1,
-        onDrop: (acceptedFiles) => {
+        onDrop: async(acceptedFiles) => {
           console.log(acceptedFiles);
+          const file = acceptedFiles[0];
+          if (file.size > 10 * 1024 * 1024) {
+            // bigger than 10mb!
+            alert("File is too big! Please pick a smaller file");
+            return;
+          }
+
+          try{
+          const data = await uploadToS3(file);
+          console.log('data',data);
+          }
+          catch(error)
+          {
+            console.log(error);
+          }
           },
 });
     
