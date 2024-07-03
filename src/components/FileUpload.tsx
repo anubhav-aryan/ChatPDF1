bea426f8433668213f9e7bda81f55350175ee5bc
Nishchay Sinha <61420072+nishchaysinha@users.noreply.github.com>
2024-07-04 03:38:06 +0530

    BLEH

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,17 +1,19 @@
 "use client";
-import React from 'react'
-import { useDropzone } from 'react-dropzone';
-import { Inbox, Loader2 } from 'lucide-react';
-import { uploadToS3 } from '@/lib/s3';
-import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
+import { uploadToS3 } from "@/lib/s3";
+import { useMutation } from "@tanstack/react-query";
+import { Inbox, Loader2 } from "lucide-react";
+import React from "react";
+import { useDropzone } from "react-dropzone";
+import axios from "axios";
 import { toast } from "react-hot-toast";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
+
+// https://github.com/aws/aws-sdk-js-v3/issues/4126
 
 const FileUpload = () => {
   const router = useRouter();
   const [uploading, setUploading] = React.useState(false);
-  const { mutate, isPending } = useMutation({
+  const { mutate, isLoading } = useMutation({
     mutationFn: async ({
       file_key,
       file_name,
@@ -27,57 +29,52 @@ const FileUpload = () => {
     },
   });
 
-    const { getRootProps, getInputProps } = useDropzone({
-        accept: { "application/pdf": [".pdf"] },
-        maxFiles: 1,
-        onDrop: async(acceptedFiles) => {
-          console.log(acceptedFiles);
-          const file = acceptedFiles[0];
-          if (file.size > 10 * 1024 * 1024) {
-            // bigger than 10mb!
-            toast.error("File too large");
-            return;
-          }
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { "application/pdf": [".pdf"] },
+    maxFiles: 1,
+    onDrop: async (acceptedFiles) => {
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        // bigger than 10mb!
+        toast.error("File too large");
+        return;
+      }
 
-          try{
-          setUploading(true);
-          const data = await uploadToS3(file);
-          if(!data?.file_key || !data.file_name){
-            toast.error("Something went wrong! Please try again later");
-            return;
-          }
-          mutate(data, {
-            onSuccess: ({chat_id}) => {
-              toast.success('Chat created successfully');
-              router.push(`/chat/${chat_id}`);
-            },
-            onError: (error) => {
-              toast.error("Error creating chat");
-              console.error(error);
-            },
-          
-          })
-          }
-          catch(error)
-          {
-            console.log(error);
-          }
-          finally{
-            setUploading(false);
-          }
+      try {
+        setUploading(true);
+        const data = await uploadToS3(file);
+        console.log("meow", data);
+        if (!data?.file_key || !data.file_name) {
+          toast.error("Something went wrong");
+          return;
+        }
+        mutate(data, {
+          onSuccess: ({ chat_id }) => {
+            toast.success("Chat created!");
+            router.push(`/chat/${chat_id}`);
           },
-});
-    
+          onError: (err) => {
+            toast.error("Error creating chat");
+            console.error(err);
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setUploading(false);
+      }
+    },
+  });
   return (
     <div className="p-2 bg-white rounded-xl">
-        <div
+      <div
         {...getRootProps({
           className:
             "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
         })}
       >
         <input {...getInputProps()} />
-        {uploading || isPending ? (
+        {uploading || isLoading ? (
           <>
             {/* loading state */}
             <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
@@ -85,14 +82,15 @@ const FileUpload = () => {
               Spilling Tea to GPT...
             </p>
           </>
-        ):(
+        ) : (
           <>
             <Inbox className="w-10 h-10 text-blue-500" />
             <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
-          </>)}
+          </>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FileUpload
+export default FileUpload;
